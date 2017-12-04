0daa5a80e8762696a04bd6fa20cb288eb30aef22
xhlf <zhengxin0913@vip.qq.com>
2017-12-04 18:22:14 +0800

    守护进程

---
+++ daemon.js
@@ -0,0 +1,22 @@
+// TODO: 守护进程工作方式
+const cp = require('child_process');
+let worker;
+
+function spawn(server, config) {
+  worker = cp.spawn('node', [server, config]);
+  worker.on('exit', function(code) {
+    if (code !== 0) {
+      spawn(server, config);
+    }
+  });
+}
+
+function main(argv) {
+  spawn('index3.js', argv[0]);
+  process.on('SIGTERM', function() {
+    worker.kill();
+    process.exit(0);
+  });
+}
+
+main(process.argv.slice(2));
